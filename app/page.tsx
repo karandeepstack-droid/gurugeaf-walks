e6a832c58bd3d190f5e1aa3484dde3c9bd54f775
Karandeep Singh <karandeepsingh@Mac.bbrouter>
2026-06-18 14:57:03 +0530

    fix: make navbar clickable on all pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,47 @@
 import Image from "next/image";
 
+function NavOverlay() {
+  return (
+    <div className="absolute top-[2%] right-[5%] w-[45%] h-[5%] flex justify-between z-20">
+      <a href="#home" className="w-1/4 h-full block cursor-pointer hover:bg-black/5 rounded" title="Home">
+        <span className="sr-only">Home</span>
+      </a>
+      <a href="#about" className="w-1/4 h-full block cursor-pointer hover:bg-black/5 rounded" title="About">
+        <span className="sr-only">About</span>
+      </a>
+      <a href="#walks" className="w-1/4 h-full block cursor-pointer hover:bg-black/5 rounded" title="Walks">
+        <span className="sr-only">Walks</span>
+      </a>
+      <a href="#bookings" className="w-1/4 h-full block cursor-pointer hover:bg-black/5 rounded" title="Bookings">
+        <span className="sr-only">Bookings</span>
+      </a>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center w-full max-w-4xl mx-auto shadow-2xl bg-white min-h-screen scroll-smooth">
       
-      {/* Home Page (Page 1) with Navigation Overlay */}
       <div id="home" className="relative w-full">
         <img src="/page_0.png" className="w-full h-auto block" alt="Home" />
-        
-        {/* Invisible Navigation Overlay */}
-        <div className="absolute top-[2%] right-[5%] w-[45%] h-[5%] flex justify-between z-20">
-          <a href="#home" className="w-1/4 h-full block cursor-pointer hover:bg-black/5 rounded" title="Home">
-            <span className="sr-only">Home</span>
-          </a>
-          <a href="#about" className="w-1/4 h-full block cursor-pointer hover:bg-black/5 rounded" title="About">
-            <span className="sr-only">About</span>
-          </a>
-          <a href="#walks" className="w-1/4 h-full block cursor-pointer hover:bg-black/5 rounded" title="Walks">
-            <span className="sr-only">Walks</span>
-          </a>
-          <a href="#bookings" className="w-1/4 h-full block cursor-pointer hover:bg-black/5 rounded" title="Bookings">
-            <span className="sr-only">Bookings</span>
-          </a>
-        </div>
+        <NavOverlay />
       </div>
 
-      <div id="about" className="w-full">
+      <div id="about" className="relative w-full">
         <img src="/page_1.png" className="w-full h-auto block" alt="About" />
+        <NavOverlay />
       </div>
       
-      <div id="walks" className="w-full">
+      <div id="walks" className="relative w-full">
         <img src="/page_2.png" className="w-full h-auto block" alt="Walks" />
+        <NavOverlay />
       </div>
       
       {/* Bookings Page */}
       <div id="bookings" className="relative w-full">
         <img src="/page_3.png" className="w-full h-auto block" alt="Bookings" />
+        <NavOverlay />
         
         {/* Invisible Clickable Area over the Form to open WhatsApp */}
         <a 
@@ -50,8 +56,9 @@ export default function Home() {
         </a>
       </div>
 
-      <div id="info" className="w-full">
+      <div id="info" className="relative w-full">
         <img src="/page_4.png" className="w-full h-auto block" alt="Info" />
+        <NavOverlay />
       </div>
     </div>
   );
